f8185b0541f6a556562501cceb30c17cfd63d305
Pawan <pawansingh9@gmail.com>
2015-04-18 16:22:31 -0500

    New Files added

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 
 gem 'rails',                '4.2.0'
 gem 'bcrypt',               '3.1.7'
+gem 'faker',        '1.4.2'
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 gem 'bootstrap-sass',       '3.2.0.0'
 gem 'sass-rails',           '5.0.1'
 gem 'uglifier',             '2.5.3'
@@ -22,9 +25,13 @@ group :test do
   gem 'minitest-reporters', '1.0.5'
   gem 'mini_backtrace',     '0.1.3'
   gem 'guard-minitest',     '2.3.1'
+  gem 'cucumber-rails', :require => false
+  gem 'database_cleaner'
+  gem 'rspec-rails'
 end
 
 group :production do
   gem 'pg',             '0.17.1'
   gem 'rails_12factor', '0.0.2'
+  gem 'puma',           '2.11.1'
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:show, :edit, :update]
+  before_action :admin_user,     only: [:index, :destroy]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -8,7 +16,6 @@ class UsersController < ApplicationController
   end
   
   def create
-    #@user = User.new(params[:user])    # Not the final implementation!
     @user = User.new(user_params)
     if @user.save
        log_in @user
@@ -19,11 +26,29 @@ class UsersController < ApplicationController
     end
   end
   
-  private
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  private
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-  
 end
